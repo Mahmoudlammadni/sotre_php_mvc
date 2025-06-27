a239f21bb692eeed3311f293aa528127d663023e
Mahmoud <lamm.mah2002@gmail.com>
2025-06-27 20:20:53 +0200

    schema of databases -the code of creating-

---
+++ database/schema.sql
@@ -0,0 +1,80 @@
+CREATE DATABASE store_mvc;
+use store_mvc ;
+CREATE TABLE roles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) UNIQUE NOT NULL
+);
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(100) UNIQUE NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL, 
+  role_id INT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+CREATE TABLE categories (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  description TEXT
+);
+CREATE TABLE suppliers (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  contact_name VARCHAR(100),
+  phone VARCHAR(20),
+  email VARCHAR(100),
+  address TEXT
+);
+CREATE TABLE products (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  price DECIMAL(10,2) NOT NULL,
+  quantity INT DEFAULT 0,
+  category_id INT,
+  supplier_id INT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (category_id) REFERENCES categories(id),
+  FOREIGN KEY (supplier_id) REFERENCES suppliers(id)
+);
+CREATE TABLE products_image (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT NOT NULL,
+  image_path VARCHAR(255), 
+  is_main BOOLEAN DEFAULT FALSE,
+  FOREIGN KEY (product_id) REFERENCES products(id)
+);
+CREATE TABLE clients (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100),
+  phone VARCHAR(20),
+  address TEXT,
+  user_id INT UNIQUE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE orders (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  client_id INT,
+  user_id INT, -- who created the order
+  order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+  total_amount DECIMAL(10,2),
+  status ENUM('pending', 'paid', 'cancelled') DEFAULT 'pending',
+  FOREIGN KEY (client_id) REFERENCES clients(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+CREATE TABLE order_items (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  order_id INT,
+  product_id INT,
+  quantity INT NOT NULL,
+  unit_price DECIMAL(10,2) NOT NULL,
+  subtotal DECIMAL(10,2) NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES orders(id),
+  FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+
+
