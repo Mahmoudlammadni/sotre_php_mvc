77c28f39a2eb033c4a49aa9f84be0e3e6d607761
Mahmoud <lamm.mah2002@gmail.com>
2025-06-28 17:30:47 +0200

    Implement user login function with email and password verification in User model

--- model/User.php
+++ model/User.php
@@ -11,5 +11,16 @@ class User{
         $all->execute(); 
         return $all->fetchAll(PDO::FETCH_ASSOC);   
     }
-    
+
+    public function LogIn($email,$password){
+        $req = $this->pdo->prepare("select users.* , roles.name as role_name from users inner join roles on users.role_id = roles.id
+         where users.email=:email and users.password=:password");
+        $req->execute(['email'=>$email ,"password"=>$password]);
+        $user=$req->fetch(PDO::FETCH_ASSOC);
+        if($user){
+            return $user ;
+        }
+        return false ;
+        
+    }
 }
