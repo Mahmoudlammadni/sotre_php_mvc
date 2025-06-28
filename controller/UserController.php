94a5573944528352d7b7f008daad5df52fb6eb3a
Mahmoud <lamm.mah2002@gmail.com>
2025-06-28 17:32:09 +0200

    Add login action to handle user authentication and showLogin method to display login form

--- controller/UserController.php
+++ controller/UserController.php
@@ -11,7 +11,37 @@ class UserController{
     public function index(){
         $users=$this->model->all_users();
         include __DIR__ . '/../view/users/index.php';
-
     }
+
+
+   public function login() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $email = $_POST['email'] ?? '';
+        $password = $_POST['password'] ?? '';
+
+        $user = $this->model->LogIn($email, $password);
+
+        if ($user) {
+            $_SESSION['user'] = $user;
+
+            if ($user['role_name'] === 'admin') {
+                include __DIR__ . '/../view/admin/index.php';
+            } elseif ($user['role_name'] === 'client') {
+                include __DIR__ . '/../view/client/index.php';
+            } else {
+                echo "Role not handled.";
+            }
+        } else {
+            echo "Invalid credentials.";
+        }
+    } else {
+        echo "Invalid request method.";
+    }
+}
+
+    public function showLogin() {
+    include __DIR__ . '/../view/auth/login.php';
+}
+
 }
 
