40a64f42290146ce3b7b036a08aeaa402aad484a
Mahmoud <lamm.mah2002@gmail.com>
2025-06-30 17:40:32 +0200

    ProductController is Created with the functio store

---
+++ controller/ProductController.php
@@ -0,0 +1,44 @@
+<?php
+include  __DIR__ . "/../model/Product.php";
+ if (!isset($_SESSION['user']) || !in_array($_SESSION['user']['role_name'], ['admin', 'manager'])) {
+     echo "Unauthorized access.";
+     exit;
+ }
+class ProductController{
+ private  $model;
+ public function __construct()
+ {
+    global $pdo;
+    $this->model= new Product($pdo);
+ }
+
+  public function store($data, $file) {
+        try {
+            if (isset($file['image']) && $file['image']['error'] == 0) {
+                $uploadDir = "uploads/";
+                if (!is_dir($uploadDir)) mkdir($uploadDir, 0777, true);
+
+                $imageName = uniqid() . '_' . basename($file['image']['name']);
+                $uploadPath = $uploadDir . $imageName;
+
+                move_uploaded_file($file['image']['tmp_name'], $uploadPath);
+            } else {
+                throw new Exception("Image upload failed.");
+            }
+
+          
+            $relativeImagePath = $uploadDir . $imageName;
+            $this->model->insertProductWithImage($data, $relativeImagePath);
+
+            echo "Product saved successfully.";
+            header("Location: /sotre_php_mvc/index.php?controller=home&action=index");
+            exit;
+
+
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+  }}
+
+
+?>
