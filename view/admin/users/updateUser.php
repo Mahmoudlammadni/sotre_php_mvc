7876100d2327520bc1de26b3a8f78c4e42427a74
Mahmoud <lamm.mah2002@gmail.com>
2025-07-06 01:09:57 +0200

    add the view to update user

--- view/admin/users/updateUser.php
+++ view/admin/users/updateUser.php
@@ -4,10 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/sotre_php_mvc/public/css/form.css">
 </head>
 <body>
-    <?php var_dump($user);
-     ?>
+ 
     
+<h2 style="text-align: center;">Edit User</h2>
+
+<form action="index.php?controller=user&action=update&id=<?= $user['id'] ?>" method="post" >
+    <label for="username"> Name:</label>
+    <input type="text" name="username" value="<?= htmlspecialchars($user['username']) ?>" required>
+
+    <label for="email">email:</label>
+    <textarea name="email" rows="4" required><?= htmlspecialchars($user['email']) ?></textarea>
+
+    <label for="user">password</label>
+    <input type="text" name="password" value="<?= htmlspecialchars($user['password']) ?>" required>
+
+   <select name="role_id" required>
+    <option value="" disabled>-- Select role --</option>
+    <?php foreach ($roles as $r): ?>
+        <?php if ($r['name'] !== 'client') : ?>
+            <option value="<?= $r['id'] ?>" <?= $r['id'] == $user['role_id'] ? 'selected' : '' ?>>
+                <?= htmlspecialchars($r['name']) ?>
+            </option>
+        <?php endif; ?>
+    <?php endforeach; ?>
+</select>
+
+   
+
+    <button type="submit">Update User</button>
+</form>
 </body>
 </html>
