b485ad9755aab2dae478575b637d140143d3c1ab
Mahmoud <lamm.mah2002@gmail.com>
2025-07-06 22:47:44 +0200

    function udpate for client

--- controller/ClientController.php
+++ controller/ClientController.php
@@ -22,7 +22,22 @@ class ClientController{
         include __DIR__ ."/../view/admin/clients/edite.php";
     }
 
+    public function update(){
+        $id = $_GET['id'] ;
+        if (!$id) {
+              echo "No client ID provided.";
+        return;}
+        $name = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $phone = $_POST["phone"];
+        $address = $_POST["address"];
+        $this->model->UpdateClient($id,$name,$email,$password,$phone,$address);
+        header("Location: index.php?controller=client&action=index");
 
+    }
+
+    
     public function destroy(){
         if(isset($_GET["id"])){
             $id=$_GET["id"];
--- view/admin/clients/edite.php
+++ view/admin/clients/edite.php
@@ -4,9 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/sotre_php_mvc/public/css/form.css">
+
 </head>
 <body>
-    <?php var_dump($client) ?>
-<h1>heloo from update page </h1>
+   
+
+   
+<h2 style="text-align: center;">Edit Client</h2>
+
+<form action="index.php?controller=client&action=update&id=<?= $client['user_id'] ?>" method="post" >
+    <label for="username"> Name:</label>
+    <input type="text" name="username" value="<?= htmlspecialchars($client['username']) ?>" required>
+
+    <label for="email">email:</label>
+    <textarea name="email" rows="4" required><?= htmlspecialchars($client['email']) ?></textarea>
+
+    <label for="user">password</label>
+    <input type="text" name="password" value="<?= htmlspecialchars($client['password']) ?>" required>
+    
+      <label for="phone">phone</label>
+    <input type="text" name="phone" value="<?= htmlspecialchars($client['phone']) ?>" required>
+    
+      <label for="address">address</label>
+    <input type="text" name="address" value="<?= htmlspecialchars($client['address']) ?>" required>
+
+    <button type="submit">Update Client</button>
+</form>
+
 </body>
 </html>
