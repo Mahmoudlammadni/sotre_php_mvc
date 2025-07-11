d9e94da98a06ec8f588b93e10b259a374bc41d34
Mahmoud <lamm.mah2002@gmail.com>
2025-07-11 23:43:28 +0200

    add the CartController to manage added products by session

---
+++ controller/CartController.php
@@ -0,0 +1,36 @@
+<?php
+class CartController {
+
+    public function add() {
+        if (!isset($_SESSION['user'])) {
+            $_SESSION['redirect_to'] = $_SERVER['REQUEST_URI'];
+            header("Location: index.php?controller=user&action=showLogin");
+            exit;
+        }
+
+        $productId = $_GET['id'];
+        $quantity = $_GET['quantity'] ?? 1;
+
+        if (!isset($_SESSION['cart'])) {
+            $_SESSION['cart'] = [];
+        }
+
+        if (isset($_SESSION['cart'][$productId])) {
+            $_SESSION['cart'][$productId] += $quantity;
+        } else {
+            $_SESSION['cart'][$productId] = $quantity;
+        }
+
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            header('Content-Type: application/json');
+            echo json_encode([
+                'success' => true,
+                'cart_count' => array_sum($_SESSION['cart'])
+            ]);
+            exit;
+        }
+
+        header("Location: " . ($_SERVER['HTTP_REFERER'] ?? 'index.php'));
+    }
+
+}
